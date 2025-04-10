cb631ca02647b3fa7845bf69afd0e7830eb81d4b
Zuha Rosario <zuhalourenco@gmail.com>
2025-04-10 20:46:53 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LP1Semana08
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
